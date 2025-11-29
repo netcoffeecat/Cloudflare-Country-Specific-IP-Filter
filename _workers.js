aa82b31cdf9022adf77b9a9f3aa88ec6dd47cfb9
AlienWareGF <49101704+alienwaregf@users.noreply.github.com>
2025-11-29 10:59:32 +0800

    Update _workers.js

--- _workers.js
+++ _workers.js
@@ -107,7 +107,7 @@ export default {
     const html = `<!DOCTYPE html>
   <html lang="zh-CN">
   <head>
-  <link rel="icon" href="https://www.cloudflare.com/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="https://raw.githubusercontent.com/alienwaregf/personal-use/refs/heads/main/image/Favicon/GF.svg" type="image/svg+xml">
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Cloudflare IP 收集器</title>
